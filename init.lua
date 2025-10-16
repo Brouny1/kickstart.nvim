fad58b259bd2d366fb3cd59860d5a00856374aab
Brouny1 <fuerbrouny@gmail.com>
2025-10-16 10:42:00 +0200

    removed tab keybind with blink cmp. added $ auto closing for typst files

--- init.lua
+++ init.lua
@@ -905,7 +905,8 @@ require('lazy').setup({
         --
         -- See :h blink-cmp-config-keymap for defining your own keymap
         preset = 'default',
-
+        ['<Tab>'] = {}, -- to stop jumping to last snippet, when pressing tab. ei. to stop overloading the functionality of the tab key.
+        ['<S-Tab>'] = {},
         -- For more advanced Luasnip keymaps (e.g. selecting choice nodes, expansion) see:
         --    https://github.com/L3MON4D3/LuaSnip?tab=readme-ov-file#keymaps
       },
--- lua/kickstart/plugins/autopairs.lua
+++ lua/kickstart/plugins/autopairs.lua
@@ -5,4 +5,15 @@ return {
   'windwp/nvim-autopairs',
   event = 'InsertEnter',
   opts = {},
+
+  config = function(_, opts)
+    local npairs = require 'nvim-autopairs'
+    npairs.setup(opts)
+    local Rule = require 'nvim-autopairs.rule'
+    npairs.add_rules {
+      Rule('$', ' $', { 'typst', 'typ' }) -- or "*" for all
+        :with_move()
+        :use_key '$',
+    }
+  end,
 }

b45afde0d75645cfb4f7d4fa7df1fa6c88bcd12c
Brouny1 <fuerbrouny@gmail.com>
2026-02-03 18:52:08 +0100

    Update plugins with lazy. Now (hopefully) works

--- init.lua
+++ init.lua
@@ -977,6 +977,7 @@ require('lazy').setup({
 
   { -- Highlight, edit, and navigate code
     'nvim-treesitter/nvim-treesitter',
+    -- branch = 'main',
     config = function()
       local filetypes = { 'bash', 'c', 'diff', 'html', 'lua', 'luadoc', 'markdown', 'markdown_inline', 'query', 'vim', 'vimdoc' }
       require('nvim-treesitter').install(filetypes)
